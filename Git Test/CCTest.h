5876dde9a22431c563a58eff4386f29711232ea9
Reggie Tucker <Reggie@Reggie-Tuckers-MacBook-Pro.local>
2014-04-05 23:40:10 -0400

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Reggie Tucker on 4/5/14.
+//  Copyright (c) 2014 B3B. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
